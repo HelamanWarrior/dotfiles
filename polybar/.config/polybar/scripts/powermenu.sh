c7fadfe2a2b39c6ad7fb9899a2111a10dcd84237
PlugWorld <40321938+HelamanWarrior@users.noreply.github.com>
2022-06-06 08:50:45 -0600

    Fixed PowerMenu

--- polybar/.config/polybar/scripts/powermenu.sh
+++ polybar/.config/polybar/scripts/powermenu.sh
@@ -3,8 +3,8 @@
 chosen=$(printf " Power Off\n勒 Restart\n Lock" | rofi -dmenu -i -theme-str '@import "power.rasi"')
 
 case "$chosen" in
-  "Power Off") shutdown now ;;
-  "Restart") reboot ;;
-  "Lock") killall bspwm ;;
+  " Power Off") poweroff ;;
+  "勒 Restart") reboot ;;
+  " Lock") killall bspwm ;;
   *) exit 1 ;;
 esac
